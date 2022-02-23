7a4764f35cc6fb7e8b219e5644df03e97ab041fd
ChonlananT <ciwerxil@gmail.com>
2022-02-23 16:13:02 +0700

    เพิ่มระบบบอส + ระบบไอเทมแบบกดใช้ + แก้ไขบัค + เงื่อนไขจบเกม

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <ctime>
 
+
 using namespace std;
 
 void HUD();
@@ -12,13 +13,14 @@ void Moving();
 void CreateMonster();
 void LevelUp();
 
-string name = " ";
-int level = 0, xp = 0, health = 0, totalHealth = 0, maxHealth = 0, nextLevel = 0, heal = 0,atk=8;
-int scalpelState = 0, ppeState = 0,SPcooldown = 0, Scalpel=0, PPE=0;
 
+
+string name = " ";
+int level = 0, xp = 0, health = 0, totalHealth = 0, maxHealth = 0, nextLevel = 0, heal = 0, currentRoom=1, healthPotion=0, chemicalX=0, atk = 8;
+int scalpelState = 0, ppeState = 0, Scalpel=0, PPE=0, SPcooldown=0;
 string monsterName[] = {"Nurse","Researcher","Doctor","Psychologist","Director","Janitor","Security guard"}; //ถ้าคิดตัวไรออกอีกก็เพิ่มได้ (ป้อง)
 string currentMonster = " ";
-int monsterHp = 0, monsterXp = 0, monsterLevel = 0;
+int monsterHp = 0, monsterXp , monsterLevel ;
 
 int main(){
     
@@ -28,8 +30,8 @@ int main(){
     xp = 0;
     nextLevel = 76;
     health = 100;
-    totalHealth = health;
-    maxHealth = totalHealth;
+    totalHealth = health + ::PPE;
+    maxHealth = totalHealth + ::PPE;
 
 
 
@@ -39,13 +41,13 @@ int main(){
 
     //หน้าโหลดหลังจากสร้างตัวละครเสร็จ(ทำให้มีดูมีลูกเล่นมากขึ้นนิดนึง5555555)
     cout << "Creating Charecter.\n";
-    Sleep(350);
+    Sleep(500);
     system("cls");
     cout << "Creating Charecter..\n";
-    Sleep(350);
+    Sleep(500);
     system("cls");
     cout << "Creating Charecter...\n";
-    Sleep(350);
+    Sleep(500);
     system("cls");
 
     
@@ -62,7 +64,7 @@ int main(){
 void HUD() { 
     Sleep(500);
     system("cls");
-    cout << "Name: " << name << "       Health: " << totalHealth+::PPE << "\nLevel: " << level << "\nExp: " << xp << "\nExp to next level: " << nextLevel << "\n";
+    cout << "Name: " << name << "       Health: " << totalHealth+::PPE << "\nLevel: " << level << "\nExp: " << xp << "\nExp to next level: " << nextLevel << "\n" << "Current room: " << ::currentRoom;
 }
 
 //ตัวเลือกว่าจะทำอะไรต่อในหน้าหลัก (เพิ่มได้ & แก้ได้)
@@ -71,29 +73,63 @@ void Moving() {
     cout << "\n\n";
     cout << "1. Go to next room\n";
     cout << "2. Open inventory\n";
-    cout << "3. Stay in current room\n";
+    cout << "3. Explore this room\n";
     cout << "\n";
     
     cin >> choice;
     if(choice == 1){
         int temp = rand()%100 + 1; 
-        cout << "You begin moving to next room...\n";
-        if(temp >= 50 ) { 
+        system("cls");
+        cout << "You begin moving to the next room.\n";
+        Sleep(500);
+        system("cls");
+        cout << "You begin moving to the next room..\n";
+        Sleep(500);
+        system("cls");
+        cout << "You begin moving to the next room...\n";
+        Sleep(500);
+        system("cls");
+       
+        ::currentRoom++;
+        if(temp <= 80) { 
+            if(currentRoom < 20){
             CreateMonster();
             string monster = monsterName[rand()%6];
             cout << "You found the " << monster << "! Prepare to fight!\n";
             currentMonster = monster;
-            Sleep(1000);
+            Sleep(1500);
             Combat();
-            
+            }else{ // สร้าง BOSS
+                CreateMonster();
+                monsterLevel = level;
+                monsterHp = maxHealth+120;
+                string monster = monsterName[6];
+                cout << "You found the last boss " << monster << "! Prepare to your final fight!\n";
+                currentMonster = monster;
+                Sleep(2000);
+                Combat();
+            } 
         }
         else {
             //เพิ่มEventอื่นนอกจากเจอมอน ว่านทำเอง
+            int temp = rand()%2 + 1; 
+            if(temp == 1){
+            cout << "Congratulations!!! You found a Medkit\n";
+            ::healthPotion++;
+            Sleep(1500);
             HUD();
             Moving();
+            }else {
+                cout << "Congratulations!!! You found a Chemical X\n";
+                ::chemicalX++;
+                Sleep(1500);
+                HUD();
+                Moving();        
+            }
         }
     }
     else if(choice == 2){
+        
         system("cls");
         int ans;
         cout << "Please select your equipment\n\n";
@@ -154,8 +190,7 @@ void Moving() {
             HUD();
             Moving();
         }
-        
-    }
+    }    
     else if(choice == 3){
         system("cls");
         cout << "You begin exploring this room.\n";
@@ -211,7 +246,12 @@ void Moving() {
             Combat();
         }
     }
-} 
+    else{
+        cout << "Wrong Input!!!";
+        Sleep(500);
+        Moving();
+    } 
+}
 
 void CreateMonster(){ // ระบบสุ่มเกิดมอน พี่เธียรปรับได้ (stat สุ่มของ Monster) เพราะตอนนี้ยังไม่ค่อยบาลานซ์
     monsterLevel = (rand()%3) + level;
@@ -220,7 +260,6 @@ void CreateMonster(){ // ระบบสุ่มเกิดมอน พี่
     if (monsterHp == 0 ) CreateMonster();
     if (monsterLevel == 0) CreateMonster();
 }
-
 
 void CombatHUD(){
     Sleep(500);
@@ -247,7 +286,9 @@ void Combat(){
 
         if(playerAttack == 1){
             //ตี
-            cout << "Attacking... you did " << playerDamage << " to the " << currentMonster << "\n";
+            cout << "Attacking...\n";
+            Sleep(500);
+            cout << "you did " << playerDamage << " damage to the " << currentMonster << "\n";
             monsterHp = monsterHp - playerDamage;
             Sleep(1250);
             CombatHUD();
@@ -266,19 +307,27 @@ void Combat(){
                     exit(0);
                 }
             }else if(monsterHp <= 0) {
+                if(currentRoom >= 20){
+                    system("cls");
+                    cout << "Congrats eiei\n";
+                    cout << ""; // เพิ่มบทตอนฉากจบให้หน่อย
+                    Sleep(3000); 
+                    exit(0);
+                }
                 monsterHp = 0;
-                LevelUp();
-                cout << "\n";
+                cout << "\n\n";
+                monsterXp = 19 * monsterLevel;
                 cout << "You Defeated " << currentMonster << " you are awarded with " << monsterXp << " Exp.\n";
                 cout << "Nice :)";
+                monsterXp = 0;
+                LevelUp();
                 Sleep(2500);
                 HUD();
                 Moving();
             }
             Sleep(1500);
             Combat();
-        }
-        else if(playerAttack == 2){
+        }else if(playerAttack == 2){
             if(::SPcooldown == 0){ 
                 if(level >= 4){
                     ::SPcooldown++;
@@ -312,7 +361,13 @@ void Combat(){
                                 exit(0);
                             }
                     }else if(monsterHp <= 0){
-                        
+                        if(currentRoom >= 20){
+                            system("cls");
+                            cout << "Congrats eiei\n";
+                            cout << ""; // เพิ่มบทตอนฉากจบให้หน่อย
+                            Sleep(3000); 
+                            exit(0);
+                        }
                         monsterHp = 0;
                         cout << "\n\n";
                         monsterXp = 19 * monsterLevel;
@@ -338,29 +393,85 @@ void Combat(){
                 cout << "You can use the Special Attack only once per round.\n";
                 Sleep(1500);
                 Combat();       
-            }
-        }
+            }    
+        } 
         else if(playerAttack == 3){
+            system("cls");
+            int ans;
+            cout << "Please select your items\n\n";
+            cout << "1. Health potion\n";
+            cout << "2. Chemical X\n";
+            cin >> ans;
+            if(ans == 1){
+                if(::healthPotion >= 1){
+                    ::healthPotion--;
+                    cout << "\nYour HP is restored by 100\n";
+                    totalHealth = totalHealth + 100;
+                    if(totalHealth >= maxHealth) totalHealth = maxHealth;
+                    Sleep(1500);
+                    Combat();
+                }else{ 
+                    cout << "You don't have any Health potion left\n";
+                    Sleep(1500);
+                    Combat();
+                }
+            }else if(ans == 2){
+                if(::chemicalX >= 1){
+                ::chemicalX--;
+                ::atk = atk+25;   
+                cout << "Your damage is increased by 25. (This item effect will remain until the fight ends)\n";
+                Sleep(2500);
+                Combat();
+                }else{
+                    cout << "You don't have any Chamical X left\n";
+                    Sleep(1500);
+                    Combat();
+                }               
+            }
+            Sleep(1000);
+            Combat();
         }
         else if(playerAttack == 4){
             //วิ่งหนีแบบใส่เกียร์หมา
+            system("cls");
+            cout << "You try to run away.\n";
+            Sleep(500);
+            system("cls");
+            cout << "You try to run away..\n";
+            Sleep(500);
+            system("cls");
             cout << "You try to run away...\n";
-            Sleep(1250);
+            Sleep(500);
+            system("cls");
+           
             int x = rand() % 100 +1;
             if(x >= 70){
                 cout << "You escaped safely.\n";
+                ::currentRoom--;
+                ::SPcooldown=0;
                 Sleep(1250);
                 HUD();
                 Moving();
             }
             else {
-                cout << "Can't escape!\n";
+                cout << "Can't escape!\n\n";
+                Sleep(500);
                 cout << currentMonster << " does a savage attack on you!\n";
                 totalHealth -= monsterAttack + 5;
                 cout << "You lost " << monsterAttack + 5 << " hp and your current hp is " << totalHealth << "\n";
+                if (totalHealth <= 0){
+                    totalHealth = 0;
+                    system("cls");
+                    cout << "You DIED! \nYou were level: " << level << " you got killed by " << currentMonster << ".\n";
+                    Sleep(1000);
+                    cout << "You can't get out of the hospital. so sad :(\n";
+                    Sleep(3000); 
+                    exit(0);
+                }
                 Sleep(2000);
                 Combat();
             }
+
         }
         else {
             cout << "Wrong Input!!!";
@@ -372,6 +483,7 @@ void Combat(){
 
 void LevelUp() {
     ::atk = 8;
+    ::SPcooldown = 0;
     monsterXp = 19 * monsterLevel;
     xp = xp + monsterXp;
 
@@ -380,7 +492,7 @@ void LevelUp() {
         nextLevel = nextLevel * 3/2;
         xp = 0;
         totalHealth = totalHealth + 20;
-        maxHealth = maxHealth + 20;
+        maxHealth = maxHealth+ + 20;
         cout << "\n\n";
         cout << "Congratulations " << name << " you are now level " << level << "\n";
         cout << "You are STRONGER than before\n";
@@ -388,4 +500,3 @@ void LevelUp() {
         HUD();
     }
 }
-
